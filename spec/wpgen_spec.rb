0be647c562d84f1c558e74e507357d1186b396fd
Travis Luong <travis@travisluong.com>
2013-02-26 01:34:45 -0800

    fix new_theme and custom_post_type spec

--- spec/wpgen_spec.rb
+++ spec/wpgen_spec.rb
@@ -6,27 +6,32 @@ describe "wpgen" do
 
   describe "#new_theme" do
     it "should create initial theme directory and file system" do
-      Wpgen::Generator.new_theme "test"
+    	FileUtils.rm_r "wpgen_test_theme" if File.directory? "wpgen_test_theme"
+      Wpgen::Generator.new_theme "wpgen_test_theme"
+      Dir.glob("wpgen_test_theme/*").length.should eq 16
     end
   end
 
   describe "#custom_post_type" do
     it "accepts a parameter and returns php code for custom post type" do
-      Wpgen::Generator.custom_post_type('employee').should eq <<-EOF
+      FileUtils.cd "wpgen_test_theme"
+      Wpgen::Generator.custom_post_type('employee')
+      File.open("post-type-employee.php", "r") do |f|
+        f.read.should eq <<-EOF
 <?php
 
 // Employee custom content type
 function post_type_employee() {
-  register_post_type('employee',
-    array('label' => __('Employee'),
-    'public' => true,
-    'show_ui' => true,
-    'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail' ),
-  ));
+	register_post_type('employee',
+		array('label' => __('Employee'),
+		'public' => true,
+		'show_ui' => true,
+		'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail' ),
+	));
 }
 add_action('init', 'post_type_employee');
 EOF
+      end
     end
   end
-
 end
