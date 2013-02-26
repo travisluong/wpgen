0e674dd5e9d678fbed80c252a0291d282ba3c854
Travis Luong <travis@travisluong.com>
2013-02-26 02:17:02 -0800

    add require statement check spec

--- spec/wpgen_spec.rb
+++ spec/wpgen_spec.rb
@@ -13,6 +13,7 @@ describe "wpgen" do
   end
 
   describe "#custom_post_type" do
+
     it "accepts a parameter and returns php code for custom post type" do
       FileUtils.cd "wpgen_test_theme"
       Wpgen::Generator.custom_post_type('employee')
@@ -33,5 +34,12 @@ add_action('init', 'post_type_employee');
 EOF
       end
     end
+
+    it "should add the require statement to functions.php" do
+      File.open("functions.php", "r") do |f|
+      	(f.read =~ /require_once 'post-type-employee.php'/).should be_true
+      end
+    end
+
   end
 end
